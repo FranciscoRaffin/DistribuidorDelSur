0a29ac8e69f1bd03792537487549db89ad25e43d
FranciscoRaffin <142631559+FranciscoRaffin@users.noreply.github.com>
2024-02-17 19:01:44 -0300

    Add files via upload

---
+++ datos_catalogo.js
@@ -0,0 +1,399 @@
+
+
+
+
+var analgesicos = [
+//  |Nombre                          |Precio  
+    ['ACTRON 400 X20C',              100.5],
+    ['ACTRON 600 X20C',              100.5],
+    ['ACTRON MUJER X10C',            100.5],
+    ['ACTRON PLUS X8C',              100.5],
+    ['AGUA MED 10VS PACK24',         100.5],
+    ['ALIKAL CLASICO X30S',          100.5],
+    ['ALIKAL LIMON X30S',            100.5],
+    ['ALIKAL NARANJA X30S',          100.5],
+    ['ALMAXIMO 50MG X20C',           100.5],
+    ['AMOXIDAL 500 X80C',            100.5],
+    ['ANAFLEX PLUS X20C',            100.5],
+    ['ANAFLEX X16C',                 100.5],
+    ['ANGINETAS X30C',               100.5],
+    ['ASPIRINETAS X98C',             100.5],
+    ['ATOMO POTE GEL',               100.5],
+    ['BAYASPIRINA C CALIENTE X24S',  100.5],
+    ['BAYASPIRINA C FRIA X24S',      100.5],
+    ['BAYASPIRINA FORTE X20C',       100.5],
+    ['BAYASPIRINA X30C',             100.5],
+    ['BICARBONATO X50S',             100.5],
+    ['BUSCAPINA COMPUESTA X60',      100.5],
+    ['BUSCAPINA DUOX60',             100.5],
+    ['BUSCAPINA PERLAS X50C',        100.5],
+    ['BUSCAPINA SIMPLE X20',         100.5],
+    ['CAFIASPIRINA PLUS X20C',       100.5],
+    ['CAFIASPIRINA X30C',            100.5],
+    ['CAPS BLANDAS FABO600 X50C',    100.5],
+    ['CARAMELO GARGANTA X10C',       100.5],
+    ['CARBON X50T',                  100.5],
+    ['CEFAL EXINA X80C',             100.5],
+    ['DICLOFENAC 100 CAP TUBO X 50', 100.5],
+    ['DICLOFENAC 75MG X50T',         100.5],
+    ['DORIXINA X100C',               100.5],
+    ['FABAMOX 500 X80C',             100.5],
+    ['FABO 600 NUEVO X60',           100.5],
+    ['FEEN A MINT X20T',             100.5],
+    ['FRASCO EST ERIL GASANA',       100.5],
+    ['GASA SUEZ X10',                100.5],
+    ['GENIOL 1G X56C',               100.5],
+    ['GENIOL PLUS CAPS X12C',        100.5],
+    ['HEPA GOTAS',                   100.5],
+    ['IBU 400 HLB X50T',             100.5],
+    ['IBU 600 TRB X50T',             100.5],
+    ['IBU FORTE X20C',               100.5],
+    ['IBU MAX X20C',                 100.5],
+    ['IBU PLUS X20',                 100.5],
+    ['IBU RA X20',                   100.5],
+    ['IBUPIRAC 400 X60C',            100.5],
+    ['IBUPIRAC 600 X50C',            100.5],
+    ['IBUPIRAC GEL 400 X50C',        100.5],
+    ['JARABE 2',                     100.5],
+    ['JARABE 4',                     100.5],
+    ['KETO 10MG SUBLI X50T',         100.5],
+    ['KETO 20MG X50T',               100.5],
+    ['LATA VICKVAPORUB X12U',        100.5],
+    ['LORATADINA X10C',              100.5],
+    ['MEJORALITO X30T',              100.5],
+    ['MIGRAL 500 X100C',             100.5],
+    ['MIGRAL COMP X100C',            100.5],
+    ['MYLANTA X96C M L F',           100.5],
+    ['NEXT GRIP X10C',               100.5],
+    ['NEXT PLUS X20C',               100.5],
+    ['NEXT TE X50S',                 100.5],
+    ['NOVALGINA X10C',               100.5],
+    ['OMEPRAZOL HLB TUBOX50',        100.5],
+    ['PARACETAMOL 500 X50',          100.5],
+    ['PIRIDINOL DICLO X50',          100.5],
+    ['QURA PLUS X20C',               100.5]
+];
+
+var cordones = [
+//  |Nombre                          |Precio  
+    ['CORDON 0 60 X12 BL NE',        100.5],
+    ['CORDON 0 80 X12 BL NE',        100.5],
+    ['CORDON 1 10 X12 BL NE',        100.5]
+];
+
+var dentifricosCepillos = [
+//  |Nombre                          |Precio  
+    ['CEPILLO ADULTO BLISTER X6',    100.5],
+    ['CEPILLO ADULTO CAJA X12',      100.5],
+    ['CEPILLO COLGATE X14',          100.5],
+    ['COLGATE X70G',                 100.5],
+    ['JABALI 604 XDOCENA',           100.5],
+    ['KOLINOS X70G',                 100.5],
+    ['ODOL X90G',                    100.5]
+];
+
+var encendedores = [
+//  |Nombre                          |Precio  
+    ['BX7 X50',                      100.5],
+    ['CANDELA ELECTRONICO X20',      100.5],
+    ['CANDELA TTE X25',              100.5],
+    ['CHISPERO CANDELA X12',         100.5],
+    ['ENC VOLCAN X50',               100.5],
+    ['MAXI BIC',                     100.5],
+    ['MINI BIC',                     100.5],
+    ['SHARK TTE X25',                100.5],
+    ['T-PRENDE TTE X25',             100.5]
+];
+
+var filos = [
+//  |Nombre                          |Precio  
+    ['AFEITA BIC AMARILLA X24',      100.5],
+    ['ASTRA MINORA GILLETE X24',     100.5],
+    ['CONFORT TRES FILOS VERDE X12', 100.5],
+    ['DERMA PRESTO X10',             100.5],
+    ['GILLETE ROJA XHOJA',           100.5],
+    ['MACH 3 X2',                    100.5],
+    ['MAQUINA MACH2 RTO',            100.5],
+    ['MINORA X20',                   100.5],
+    ['PIVOT PRESTO X24',             100.5],
+    ['PRESTO 3 BOLSA X2',            100.5],
+    ['PRESTOBARBA ULTRAGRIP',        100.5],
+    ['TRES FILOS X10',               100.5],
+    ['VENUS MUJER X8',               100.5],
+];
+
+var fosforos = [
+//  |Nombre                          |Precio  
+    ['FOSFOROS PATITO X400.',        100.5],
+    ['TRES PATITOS X222.',           100.5]
+];
+
+var globosCotillon = [
+//  |Nombre                          |Precio  
+    ['BENGALAS X4',                  100.5],
+    ['BOMBUCHA BTX30',               100.5],
+    ['GLOBO AGUA DELFIN X80 BTX30',  100.5],
+    ['GLOBO CANDELA DE AGUA BTX50',  100.5],
+    ['GLOBO CANDELA FC X50',         100.5],
+    ['GLOBO CANDELA PERLADO N9X50',  100.5],
+    ['GLOBO LISO CANDELA X50',       100.5],
+    ['NUMEROS TORNEADOS X20 BLANCO', 100.5],
+    ['NUMEROS TORNEADOS X20 CELESTE',100.5],
+    ['NUMEROS TORNEADOS X20 ROSA',   100.5],
+    ['VELON TORNEADO X20 BLANCO',    100.5],
+    ['VELON TORNEADO X20 CELESTE',   100.5],
+    ['VELON TORNEADO X20 ROSA',      100.5]
+];
+
+
+var lamparas = [
+//  |Nombre                          |Precio  
+    ['LED CANDELA FRIA 12W',         100.5],
+    ['LED CANDELA FRIA 9W',          100.5]
+];
+
+var libreria = [
+//  |Nombre                          |Precio  
+    ['BOLIGRAFO BIC OPACO NEGRO X50',100.5],
+    ['BOLIGRAFO OPACO BIC AZUL X50', 100.5],
+    ['LAPIZ NEGRO BIC X144',         100.5],
+    ['VOLIGOMA PACK X12',            100.5]
+];
+
+var mosquitos = [
+//  |Nombre                          |Precio  
+    ['APARATO RAID C RTO LIQUIDO',   100.5],
+    ['APARATO VAPE INDIVIDUAL',      100.5],
+    ['ESPIRAL FUYI X48',             100.5],
+    ['ESPIRALES RAID X48',           100.5],
+    ['FUYI ESPIRAL CAJAX12',         100.5],
+    ['FUYI MYM X360',                100.5],
+    ['FUYI TABLE',                   100.5],
+    ['FUYI VAPE X12 X20',            100.5],
+    ['OFF AERO NARANJA X170G',       100.5],
+    ['OFF AERO VERDE X170G',         100.5],
+    ['OFF CREMA X60',                100.5],
+    ['RAID AZUL X360',               100.5],
+    ['RAID TABLETA X12',             100.5],
+    ['RAID TABLETA X24',             100.5]
+];
+
+var naipes = [
+//  |Nombre                          |Precio  
+    ['CASINO PLAST X40',             100.5],
+    ['CASINO PLAST X50',             100.5],
+    ['CASINO POKER X54',             100.5],
+    ['CUATRO PALOS ROJO X50',        100.5],
+    ['HABANA PLAST X40',             100.5],
+    ['HABANA PLAST X50',             100.5],
+    ['HACHAZO POKER X54',            100.5],
+    ['HACHAZO X40',                  100.5],
+    ['HACHAZO X50',                  100.5]
+];
+
+var panuelos = [
+//  |Nombre                          |Precio  
+    ['CARILINA X6',                  100.5]
+];
+
+var pegamentos = [
+//  |Nombre                          |Precio  
+    ['ALTA TEMPERATURA FASTIX',      100.5],
+    ['ECCOLE',                       100.5],
+    ['EL PULPITO PACK X6',           100.5],
+    ['FASTIX TTE',                   100.5],
+    ['LA GOTITA GEL',                100.5],
+    ['LA GOTITA',                    100.5],
+    ['POXILINA',                     100.5],
+    ['POXIPOL 10M',                  100.5],
+    ['POXIRAN',                      100.5],
+    ['UNIPOX',                       100.5],
+    ['WD 40 X155G',                  100.5]
+];
+
+var perfumeria = [
+//  |Nombre                          |Precio  
+    ['AXE DEO CAJA X12',             100.5],
+    ['BROCHA DE AFEITAR X6',         100.5],
+    ['CREMA HINDS X125G',            100.5],
+    ['CREMOSA DOREE X20VS',          100.5],
+    ['CREMOSA DOREE X30VS',          100.5],
+    ['CURITAS PACKX24',              100.5],
+    ['DOVE AERO CAJA X12',           100.5],
+    ['DOVE ROLLON ORIGINAL X12',     100.5],
+    ['EFICIENT 100G PACKX12',        100.5],
+    ['ESPUMA NIVEA 200ML PACKX6',    100.5],
+    ['GEL DOREE POMO AZ RO BC AM',   100.5],
+    ['IMPULSE DEO CAJA X12',         100.5],
+    ['JABON DOVE X90G',              100.5],
+    ['JABON LUX X125G',              100.5],
+    ['JABON REXONA X125G',           100.5],
+    ['LORCHESELINE POMO X12',        100.5],
+    ['LUYAGUS MANTECA X10',          100.5],
+    ['ODORONO CREMA PACK X12',       100.5],
+    ['PACK JABON REXONA X3',         100.5],
+    ['PATRICHS CAJA X12',            100.5],
+    ['PINZA OGIB NEGRA X12',         100.5],
+    ['REXONA FEM CAJA X12',          100.5],
+    ['REXONA MEN CAJA X12',          100.5],
+    ['REXONA ROLLON FEM X12',        100.5],
+    ['REXONA ROLLON MEN X12',        100.5],
+    ['TOALLAS HUMEDAS BELABIN X100', 100.5]
+];
+
+var pilasAlcalinas = [
+//  |Nombre                          |Precio  
+    ['DURACEL MEDIANA CAJA X16',     100.5],
+    ['DURACELL AA TROQUEL X14',      100.5],
+    ['DURACELL AAA TROQUEL X14',     100.5],
+    ['DURACELL BATERIA CAJA X12',    100.5],
+    ['DURACELL GRANDE CAJA X12',     100.5],
+    ['ENERGIZER AA TIRA X20',        100.5],
+    ['ENERGIZER AAA TIRA X20',       100.5],
+    ['ENERGIZER BATERIA CAJA X12',   100.5],
+    ['ENERGIZER GRANDE CAJA X24',    100.5],
+    ['ENERGIZER MEDIANA CAJA X24',   100.5],
+    ['ESPECIAL A23 TIRA X5',         100.5],
+    ['ESPECIAL A27 TIRA X5',         100.5],
+    ['ESPECIAL CR2032 X5',           100.5]
+];
+
+var pilasDeCarbon = [
+//  |Nombre                          |Precio  
+    ['EVEREAD',                      100.5],
+    ['EVEREADY AA',                  100.5],
+    ['EVEREADY AAA',                 100.5],
+    ['EVEREADY C',                   100.5],
+    ['EVEREADY D',                   100.5]
+];
+
+var pomadasParaCalzado = [
+//  |Nombre                          |Precio  
+    ['POMADA LIQUIDA BLANCO',        100.5],
+    ['POMADA LIQUIDA MARRON',        100.5],
+    ['POMADA LIQUIDA NEGRA',         100.5],
+    ['WASINTONG LATA MARRON',        100.5],
+    ['WASINTONG LATA NEGRA',         100.5],
+];
+
+var preservativos = [
+//  |Nombre                          |Precio  
+    ['PRIME ANATOMICO X24',          100.5],
+    ['PRIME ESPERMICIDA X24',        100.5],
+    ['PRIME EXTRALUBRICADO X24',     100.5],
+    ['PRIME MIXTA X24',              100.5],
+    ['PRIME RETARDANTE X24',         100.5],
+    ['PRIME SKYN X12',               100.5],
+    ['PRIME SUPERFINO X24',          100.5],
+    ['PRIME TACHAS X24',             100.5],
+    ['PRIME TEXTURADO X24',          100.5],
+    ['PRIME ULTRAFINO X24',          100.5],
+    ['PRIME WARMING X24',            100.5],
+    ['TUBO TULIPAN SURTIDO X12',     100.5],
+    ['TULIPAN EXHINIDOR 24+2',       100.5],
+    ['TULIPAN X16',                  100.5]
+];
+
+var quitaesmaltes = [
+//  |Nombre                          |Precio 
+    ['CUTEX X50C CJA X24',           100.5],
+    ['DOREE X60C CJAX12',            100.5],
+    ['ESMALTE LECHERITO PACK X6',    100.5],
+    ['SHAMPO/ENJUAGUE',              100.5],
+    ['SACHET ACC SEDAL X144',        100.5],
+    ['SACHET SH SEDAL X144',         100.5],
+    ['SACHETERA PANTENE ACC X24',    100.5],
+    ['SACHETERA PANTENE SH X24',     100.5],
+    ['SEDAL ACC X190ML',             100.5],
+    ['SEDAL SH X190ML',              100.5]
+];
+
+var toallasFemeninas = [
+//  |Nombre                          |Precio  
+    ['CALIPSO POCKER CALAS X50',     100.5],
+    ['LINA CALAS X48',               100.5],
+    ['POCKET DONCELLA C/A X50',      100.5],
+    ['PROTECTOR CALIPSO X30',        100.5],
+    ['PROTECTOR DONCELLA S/P X30',   100.5]
+];
+
+var varios = [
+//  |Nombre                          |Precio  
+    ['ACEITE MULTIUSO X100ML',       100.5],
+    ['AGUJA CANASTA X72',            100.5],
+    ['ALCOHOL X500ML PACKX20',       100.5],
+    ['ALGODON CARES 70G BTOX40',     100.5],
+    ['ALIKATE GRANDE TRIM',          100.5],
+    ['ANILINA X30 AZUL MARINO',      100.5],
+    ['ANILINA X30 NEGRA',            100.5],
+    ['BROCHES 12X12',                100.5],
+    ['CLIPS INVISIBLES N/R',         100.5],
+    ['GOMA HONDA FINA X12',          100.5],
+    ['GOMA HONDA GRUESA X12',        100.5],
+    ['GOMERA PLASTICA',              100.5],
+    ['GUANTE AFELPADO X12 S/M/L',    100.5],
+    ['GUANTE QUIRURJICO X100',       100.5],
+    ['HILO BARRILETE G X10',         100.5],
+    ['HILO CHORICERO G X10',         100.5],
+    ['HILO RAIA X12 BLANCO',         100.5],
+    ['HILO RAIA X12 NEGRO',          100.5],
+    ['HILO RAIA X12 SURTIDO',        100.5],
+    ['HISOPOS QSOFT X75',            100.5],
+    ['HISOPOS VASO DISMAR X100',     100.5],
+    ['LIMA CARTON N17',              100.5],
+    ['MANTECA CACAO X10',            100.5],
+    ['PALILLEROS BLISTER X12',       100.5],
+    ['PEINE BOLSILLO X12',           100.5],
+    ['PEINE CASPERO METALICO',       100.5],
+    ['PEINE CASPERO PLASTICO X12',   100.5],
+    ['PEINE FAMILIAR X12',           100.5],
+    ['TAMPON INTIMA TUBO X10 M/L',   100.5],
+    ['TELA HIPOALERGENICA 2,5 X12',  100.5],
+    ['TELA N3 X24',                  100.5],
+    ['VENDA N10 XDOCENA',            100.5],
+    ['VENDA N5 XDOCENA',             100.5],
+    ['VENDA N7 XDOCENA',             100.5]
+];
+
+
+var velas = [
+//  |Nombre                         |Precio  
+    ['VELAS RELIEVE 25X4 X160G',     100.5]
+];
+
+
+
+var catalogoMatriz = [
+//      |Ref-lista                    |Nombre en botón        |Precios visibles
+//          ↆ                             ↆ                     ↆ
+        [analgesicos,                 'Analgesicos',           false  ], 
+        [cordones,                    'Cordones',              false  ], 
+        [dentifricosCepillos,         'Dentifricos Cepillos',  true   ], 
+        [encendedores,                'Encendedores',          true   ], 
+        [filos,                       'Filos',                 true   ], 
+        [fosforos,                    'Fosforos',              true   ], 
+        [globosCotillon,              'Globos Cotillon',       true   ],
+        [lamparas,                    'Lamparas',              true   ], 
+        [libreria,                    'Libreria',              true   ], 
+        [mosquitos,                   'Mosquitos',             true   ], 
+        [naipes,                      'Naipes',                true   ], 
+        [panuelos,                    'Pañuelos',              true   ], 
+        [pegamentos,                  'Pegamentos',            true   ], 
+        [perfumeria,                  'Perfumeria',            true   ], 
+        [pilasAlcalinas,              'Pilas Alcalinas',       true   ], 
+        [pilasDeCarbon,               'Pilas De Carbon',       true   ],
+        [pomadasParaCalzado,          'Pomadas Para Calzado',  true   ], 
+        [preservativos,               'Preservativos',         true   ],
+        [quitaesmaltes,               'Quitaesmaltes',         true   ], 
+        [toallasFemeninas,            'Toallas Femeninas',     true   ],
+        [velas,                       'Velas',                 true   ],
+        [varios,                      'Varios',                true   ]
+        
+];
+
+    
+
+
+
+
+
